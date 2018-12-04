70ad6bd70412a2a95a3704ca756715c4e7045978
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-12-04 17:53:32 -0500

    Task 2.2: Add code stubs for class Map. The map class is used to hold the information for the occupancy grid information and determine frontiers from the occupancy grid of the environment

---
+++ include/et_exploration_robot/map.hpp
@@ -0,0 +1,44 @@
+// ROS Headers
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <ros/ros.h>
+
+// Grid class header file
+#include "et_exploration_robot/grid.hpp"
+
+// CPP Headers
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+class Map {
+ private:
+  uint32_t mapWidth;
+  uint32_t mapHeight;
+  float mapResolution;
+
+  geometry_msgs::Pose mapOrigin;
+  geometry_msgs::Pose gridOriginCellPose;
+
+  std::vector<std::vector<Grid>> occupancyGrid;
+
+  bool isFrontierGrid(const std::pair<uint32_t, uint32_t> &gridState);
+
+  void determineFrontierGrids();
+
+  std::vector<std::vector<std::pair<uint32_t, uint32_t>>>
+  clusterFrontierGrids();
+
+ public:
+  Map() {}
+  ~Map() {}
+
+  void initialize(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+
+  void updateOccupancyMap(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+
+  std::vector<std::vector<std::pair<uint32_t, uint32_t>>> getFrontierClusters();
+
+  std::vector<std::pair<float, float>> gridToCartesian(
+      const std::vector<std::pair<uint32_t, uint32_t>> &frontiers);
+};
---
+++ src/map.cpp
@@ -0,0 +1,33 @@
+// ROS Headers
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <ros/ros.h>
+
+// Class header files
+#include "et_exploration_robot/grid.hpp"
+#include "et_exploration_robot/map.hpp"
+
+// CPP Headers
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+Map::Map() {}
+Map::~Map() {}
+
+bool Map::isFrontierGrid(const std::pair<uint32_t, uint32_t> &gridState) {}
+
+void Map::determineFrontierGrids() {}
+
+std::vector<std::vector<std::pair<uint32_t, uint32_t>>>
+Map::clusterFrontierGrids() {}
+
+void Map::initialize(const nav_msgs::OccupancyGrid::ConstPtr &msg) {}
+
+void Map::updateOccupancyMap(const nav_msgs::OccupancyGrid::ConstPtr &msg) {}
+
+std::vector<std::vector<std::pair<uint32_t, uint32_t>>>
+Map::getFrontierClusters() {}
+
+std::vector<std::pair<float, float>> Map::gridToCartesian(
+    const std::vector<std::pair<uint32_t, uint32_t>> &frontiers) {}
