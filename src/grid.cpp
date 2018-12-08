ef6062872633045b5ec9e5a4d051a3218de7321e
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-12-08 16:00:25 -0500

    Task 3.1.1: Complete the code implementation for the methods of class Grid

--- src/grid.cpp
+++ src/grid.cpp
@@ -8,18 +8,26 @@
 Grid::Grid() {}
 Grid::~Grid() {}
 
-void Grid::setGridState(const uint32_t &height, const uint32_t &width) {}
+void Grid::setGridState(const uint32_t &height, const uint32_t &width) {
+  gridHeight_ = height;
+  gridWidth_ = width;
+}
 
-std::pair<uint32_t, uint32_t> Grid::getGridState() {}
+std::pair<uint32_t, uint32_t> Grid::getGridState() {
+  auto state = std::make_pair(gridHeight_, gridWidth_);
+  return state;
+}
 
-void Grid::updateProbability(const int8_t &updatedProbability) {}
+void Grid::updateProbability(const int8_t &updatedProbability) {
+  occupancyProbability_ = updatedProbability;
+}
 
-int8_t Grid::getProbability() {}
+int8_t Grid::getProbability() { return occupancyProbability_; }
 
-void Grid::setFrontierStatus(const bool &status) {}
+void Grid::setFrontierStatus(const bool &status) { isFrontierCell_ = status; }
 
-bool Grid::getFrontierStatus() {}
+bool Grid::getFrontierStatus() { return isFrontierCell_; }
 
-void Grid::setClusterNumber(const int &num) {}
+void Grid::setClusterNumber(const int &num) { clusterNumber_ = num; }
 
-int Grid::getClusterNumber() {}
+int Grid::getClusterNumber() { return clusterNumber_; }
