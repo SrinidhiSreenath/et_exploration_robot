b1631a9ce553f19fc0dfe1034095a73cf2cd23a3
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-12-08 18:31:09 -0500

    Task 3.2.3: Complete the source code for all methods, but the wrapper method explore, of the class Explorer

--- src/explorer.cpp
+++ src/explorer.cpp
@@ -4,7 +4,9 @@
 #include <ros/ros.h>
 
 // CPP Headers
+#include <cmath>
 #include <cstdint>
+#include <limits>
 #include <utility>
 #include <vector>
 
@@ -13,16 +15,170 @@
 #include "et_exploration_robot/grid.hpp"
 #include "et_exploration_robot/map.hpp"
 
-Explorer::Explorer(ros::NodeHandle nh) {}
+Explorer::Explorer(ros::NodeHandle nh) : n_(nh) {
+  velocityPub_ =
+      n_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 50);
+  markerPub_ =
+      n_.advertise<visualization_msgs::MarkerArray>("visualization_marker", 10);
+  mapSub_ = n_.subscribe("/map", 50, &Frontier::processMap, this);
+}
 
 Explorer::~Explorer() {}
 
-void Explorer::revolve() {}
+void Explorer::processMap(const nav_msgs::OccupancyGrid::ConstPtr &map) {
+  ROS_INFO_STREAM("Map callback");
+  if (!mapInit_) {
+    mapInit_ = true;
+    myMap.initialize(map);
+  } else {
+    myMap.updateOccupancyMap(map);
+  }
+}
 
-void Explorer::processMap(const nav_msgs::OccupancyGrid::ConstPtr &msg) {}
+void Explorer::revolve() {
+  vel_.linear.x = 0.0;
+  vel_.angular.z = -0.628319;
 
-void Explorer::determineFrontiers() {}
+  auto current = ros::Time::now();
 
-std::vector<std::pair<uint32_t, uint32_t>> getFrontiers() {}
+  ros::Time revolveTime = current + ros::Duration(10.0);
 
-void Explorer::explore() {}
+  while (ros::Time::now() < revolveTime && ros::ok()) {
+    // ROS_INFO_STREAM("Current time: " << ros::Time::now());
+    velocityPub_.publish(vel_);
+  }
+  vel_.angular.z = 0.0;
+  velocityPub_.publish(vel_);
+}
+
+void Explorer::determineFrontiers(
+    const std::vector<std::vector<std::pair<uint32_t, uint32_t>>>
+        &frontierClusters) {
+  // Calculate centroid of each cluster
+  for (auto cluster : frontierClusters) {
+    if (cluster.size() > 20) {
+      uint32_t x = 0;
+      uint32_t y = 0;
+      for (auto frontierPoint : cluster) {
+        x += frontierPoint.first;
+        y += frontierPoint.second;
+      }
+      x /= cluster.size();
+      y /= cluster.size();
+
+      auto frontier = std::make_pair(x, y);
+      frontiers_.push_back(frontier);
+    }
+  }
+}
+
+void visualizeFrontiers(
+    const std::vector<std::pair<float, float>> &frontierXY) {
+  // Visualize all frontiers
+  visualization_msgs::MarkerArray frontiersToViz;
+  frontiersToViz.markers.rsesize(frontiers_.size());
+
+  ROS_INFO_STREAM("Starting marker block");
+
+  for (size_t it = 0; it < frontiers_.size(); it++) {
+    // Set the frame ID and timestamp.  See the TF tutorials for information
+    // on these.
+    frontiersToViz.markers[it].header.frame_id = "/map";
+    frontiersToViz.markers[it].header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this frontiersToViz.markers[it].  This
+    // serves to create a unique ID Any marker sent with the same namespace
+    // and id will overwrite the old one
+    frontiersToViz.markers[it].ns = "basic_shapes";
+    frontiersToViz.markers[it].id = it;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that
+    // and SPHERE, ARROW, and CYLINDER
+    frontiersToViz.markers[it].type = shape;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS
+    // Indigo: 3 (DELETEALL)
+    frontiersToViz.markers[it].action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the frontiersToViz.markers[it].  This is a full 6DOF
+    // pose relative to the frame/time specified in the header
+    frontiersToViz.markers[it].pose.position.x = frontiersXY[it].first;
+    frontiersToViz.markers[it].pose.position.y = frontiersXY[it].second;
+    frontiersToViz.markers[it].pose.position.z = 0;
+    frontiersToViz.markers[it].pose.orientation.x = 0.0;
+    frontiersToViz.markers[it].pose.orientation.y = 0.0;
+    frontiersToViz.markers[it].pose.orientation.z = 0.0;
+    frontiersToViz.markers[it].pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    frontiersToViz.markers[it].scale.x = 0.1;
+    frontiersToViz.markers[it].scale.y = 0.1;
+    frontiersToViz.markers[it].scale.z = 0.1;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    frontiersToViz.markers[it].color.r = 0.0f;
+    frontiersToViz.markers[it].color.g = 0.0f;
+    frontiersToViz.markers[it].color.b = 1.0f;
+    frontiersToViz.markers[it].color.a = 1.0;
+
+    frontiersToViz.markers[it].lifetime = ros::Duration();
+  }
+  markerPub_.publish(frontiersToViz);
+}
+
+std::pair<float, float> closestFrontier(
+    const std::vector<std::pair<float, float>> &frontierXY) {
+  tf::StampedTransform transform;
+  try {
+    listener.lookupTransform("/map", "/base_link", ros::Time(0), transform);
+  } catch (tf::TransformException ex) {
+    ROS_ERROR("%s", ex.what());
+    ros::Duration(1.0).sleep();
+  }
+
+  auto turtleX = transform.getOrigin().x();
+  auto turtleY = transform.getOrigin().y();
+
+  float distance = std::numeric_limits<float>::max();
+
+  auto closestFrontier =
+      std::make_pair(frontierXY[0].first, frontierXY[0].second);
+
+  for (const auto &frontier : frontierXY) {
+    auto dist = std::hypot(turtleX - frontier.first, turtleY - frontier.second);
+    if (dist < distance) {
+      distance = dist;
+      closestFrontier = std::make_pair(frontier.first, frontier.second);
+    }
+  }
+  return closestFrontier;
+}
+
+void Explorer::explore() {
+  // rotate 360 in current place.
+  revolve();
+
+  // clear exisiting frontiers
+  frontiers_.clear();
+
+  // get frontier clusters from map
+  auto frontierClusters = myMap.getFrontierClusters();
+
+  // calculate frontier from clusters
+  determineFrontiers(frontierClusters);
+
+  // if frontiers exist, visualise them and calcute the closest frontier to
+  // current position of the robot.
+  if (frontiers.size() != 0) {
+    // Obtain frontiers in (X,Y) position
+    auto frontiersXY = myMap.gridToCartesian(frontiers);
+
+    // visualize the frontiers
+    visualizeFrontiers(frontiersXY);
+
+    // get the closest frontier
+    auto frontierToNavigate = closestFrontier(frontiersXY);
+  } else {
+    // done with exploration
+  }
+}
