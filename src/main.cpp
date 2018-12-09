f7f29a92236a052b9c309b5236b9a8095508293a
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-12-08 21:59:36 -0500

    Task 3.1.4: Implement the main program as a ROS node that implements the explorer class and performs the exploration task

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,24 @@
-int main(int argc, char **argv) { return 0; }
+#include <ros/ros.h>
+// Class header files
+#include "et_exploration_robot/explorer.hpp"
+#include "et_exploration_robot/grid.hpp"
+#include "et_exploration_robot/map.hpp"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "frontier_exploration");
+
+  ros::NodeHandle nh;
+  Explorer curiosity(nh);
+
+  ros::Rate loop_rate(10);
+  loop_rate.sleep();
+
+  // explore.revolve();
+  ros::spinOnce();
+  while (ros::ok()) {
+    ros::spinOnce();
+    curiosity.explore();
+  }
+
+  return 0;
+}
